996cef6edd868e8bbd2680fdc97fa63e88587e45
Renee Mumbi <1723renee@gmail.com>
2023-01-26 16:45:29 +0300

    Create fetch request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [meals, setMeals] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:3000/meals")
+    .then (res => res.json())
+    .then((data)=>setMeals(data))
+  }, [])
   return (
     <div className="App">
-          <Home/>
+          <Home meals={meals} setMeals={setMeals}/>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Form from './Form'
 
-export default function Home() {
+export default function Home({meals}) {
+    console.log(meals)
   return (
     <div><Form/></div>
   )
