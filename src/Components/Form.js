57b29d1cd9ba784d805e854db6530013497d323b
Renee Mumbi <1723renee@gmail.com>
2023-01-26 15:42:30 +0300

    Create form input fields

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Form() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    ingredients: "",
+  });
+
+  function handleChange(e) {
+    let name = e.target.name;
+    let value = e.target.value;
+    setFormData({ ...formData, [name]: value });
+  }
   return (
-    <div>Form</div>
-  )
+    <div>
+      <form className="form-control">
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            placeholder="Add food title "
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <textarea
+            onChange={handleChange}
+            className="form-control"
+            id="description"
+            rows="3"
+            name="description"
+          ></textarea>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="ingredients" className="form-label">
+            Ingredients
+          </label>
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+            id="ingredients"
+            placeholder="Add Ingredients"
+            name="ingredients"
+          />
+        </div>
+        <div className="col-auto">
+          <button type="submit" className="btn btn-primary mb-3">
+            Submit Recipe
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Form from './Form'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div><Form/></div>
   )
 }
